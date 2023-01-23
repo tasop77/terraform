58f6ea6fd3398ba451a32bafda3aa713490d2a76
Valerie Braun <valeriekristinea@gmail.com>
2023-01-23 13:50:22 -0600

    monday lecture

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "main" {
+  ami                    = data.aws_ssm_parameter.instance_ami.value
+  instance_type          = "t3.micro"
+  key_name               = "vbraun"
+  subnet_id              = aws_subnet.public[0].id
+  vpc_security_group_ids = ["sg-0339eb9af60d35c0f"]
+  tags = {
+    "Name" = "${var.default_tags.env}-EC2"
+  }
+  user_data = base64encode(file("/Users/valeriebraun/Desktop/homebrew/terraform/user.sh"))
+}
+
+output "ec2_ssh_command" {
+  value = "ssh -i vbraun.pem ubuntu@ec2-${replace(aws_instance.main.public_ip, ".", "-")}.compute-1.amazonaws.com"
+}
